76c70776de8f660f31fd49cfe8f33078716cb760
Will-Barnard-WB <141440778+Will-Barnard-WB@users.noreply.github.com>
2025-06-05 11:23:36 +0100

    Create RPNCalculator-code.hs

---
+++ RPNCalculator-code.hs
@@ -0,0 +1,27 @@
+
+-- RPN Calculator in Haskell (Interpreter)
+
+tokenize :: String -> [String]
+tokenize x = words x
+
+evaluate :: [String] -> [Int] -> Int
+evaluate [] [x] = x
+evaluate (xa:xb:xs) []
+  | xb == "-" = evaluate xs [-read(xa)]
+  | otherwise = evaluate xs ((read xb):(read xa): [])
+
+evaluate (x:xs) (y:[])
+  | x == "-" = evaluate xs ((-y):[])
+  | otherwise = evaluate xs ((read x): y:[])
+
+evaluate (x:xs) (ya:yb:ys)
+  | x == "+"  = evaluate xs ((ya + yb):ys)
+  | x == "*"  = evaluate xs ((ya * yb):ys)
+  | x == "-"  = evaluate xs (-(ya) : yb : ys)
+  | otherwise = evaluate xs (read(x):ya:yb:ys)
+
+
+-- The final function to call for RPN Calculator 
+
+run :: String -> Int
+run xs = evaluate (tokenize(xs)) []
